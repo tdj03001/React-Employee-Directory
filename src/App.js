262b2f8ef24f5c596e3699ad45a3f76a328bf289
tdj03001 <tyjenki@gmail.com>
2020-04-12 16:10:01 -0400

    added appcontext and inputchange functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import Header from "./Components/Header/header";
-import Table from "./Components/Table/table"
+import Table from "./Components/Table/table";
+import SearchInput from "./Components/SearchInput/searchInput";
+import { AppContext, EventContext } from "./AppContexts";
 import "./App.css"
 
 export default function App() {
+  const [searchText, setSearchText] = React.useState("");
 
+  const handleInputChange = (event) => {
+    setSearchText(event.currentTarget.value);
+  }
 
+  const appContext = {
+    searchText
+  }
+  const eventContext = {
+    onInputChange: handleInputChange
+  }
 
 
   return (
-
-    <div className="App">
-      <Header />
-      <Table />
-    </div>
-
+    <AppContext.Provider value={appContext}>
+      <EventContext.Provider value={eventContext}>
+        <div className="App">
+          <Header />
+          <SearchInput />
+          <Table />
+        </div>
+      </EventContext.Provider>
+    </AppContext.Provider>
   );
 }
 
