7f266898d4d0faeb157c5d3f46386ec09727c369
tdj03001 <tyjenki@gmail.com>
2020-04-12 09:04:41 -0400

    added switch cases for ascending vs descending

--- src/Components/Table/table.js
+++ src/Components/Table/table.js
@@ -6,17 +6,37 @@ import { TableContext, EventContext } from "./tableContexts";
 
 export default function Table() {
   const [sortBy, setSortBy] = React.useState();
+  const [sortOrder, setSortOrder] = React.useState();
 
   const handleTableHeaderClick = (event) => {
     const target = event.currentTarget;
     const id = target.getAttribute("data-id");
 
-    setSortBy(id);
+    if (id === sortBy) {
+      switch (sortOrder) {
+        case "Ascending":
+          setSortOrder("Descending");
+          break;
+
+        case "Descending":
+          setSortOrder(undefined);
+          setSortBy(undefined);
+          break;
+
+        default:
+
+          break;
+      }
+    } else {
+      setSortBy(id);
+      setSortOrder("Ascending");
+    }
+
 
   }
 
   const tableContext = {
-    sortBy
+    sortBy, sortOrder
   }
 
   const eventContext = {
@@ -24,26 +44,29 @@ export default function Table() {
   }
 
   let sortedData = [...data];
+
+  const changeSortOrder = sortOrder === "Ascending" ? 1 : -1;
+
   switch (sortBy) {
     case "Name":
       sortedData = sortedData.sort((a, b) => {
         const name1 = `${a.name.first} ${a.name.last}`
         const name2 = `${b.name.first} ${b.name.last}`
-        return name1.localeCompare(name2);
+        return name1.localeCompare(name2) * changeSortOrder;
       })
 
       break;
 
     case "Location":
-      sortedData = sortedData.sort((a, b) => a.location.city.localeCompare(b.location.city))
+      sortedData = sortedData.sort((a, b) => a.location.city.localeCompare(b.location.city) * changeSortOrder)
       break;
 
     case "Phone":
-      sortedData = sortedData.sort((a, b) => a.phone.localeCompare(b.phone))
+      sortedData = sortedData.sort((a, b) => a.phone.localeCompare(b.phone) * changeSortOrder)
       break;
 
     case "Email":
-      sortedData = sortedData.sort((a, b) => a.email.localeCompare(b.email))
+      sortedData = sortedData.sort((a, b) => a.email.localeCompare(b.email) * changeSortOrder)
       break;
 
     default:
